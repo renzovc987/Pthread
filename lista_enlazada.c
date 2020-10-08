b102601779ba118f329ea835cae3ba14b86ceb24
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-10-08 01:04:00 -0500

    Update lista_enlazada.c

--- lista_enlazada.c
+++ lista_enlazada.c
@@ -87,12 +87,12 @@ int Insert(int value, struct list_node_s** head_pp) {
    struct list_node_s* pred_p = NULL;
    struct list_node_s* temp_p;
    
-   while (curr_p != NULL && curr_p->data < value) {
+   while (curr_p != NULL && curr_p->data <= value) {
       pred_p = curr_p;
       curr_p = curr_p->next;
    }
 
-   if (curr_p == NULL || curr_p->data > value) {
+   if (curr_p == NULL || curr_p->data >= value) {
       temp_p = malloc(sizeof(struct list_node_s));
       temp_p->data = value;
       temp_p->next = curr_p;
@@ -102,7 +102,6 @@ int Insert(int value, struct list_node_s** head_pp) {
          pred_p->next = temp_p;
       return 1;
    } else { 
-      printf("%d is already in the list\n", value);
       return 0;
    }
 } 
