58e2b00f5e07c2b95aff7c93e3cf3d2d4254bda5
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-10-08 11:31:20 -0500

    Update leserial.c

--- leserial.c
+++ leserial.c
@@ -1,43 +1,3 @@
-/* File:     serial_linked_list .c
- * Purpose:  Implement a linked list as a Serial program
- *           Implementation should support Member( ), Insert( ), and Delete( ) functions. 
- *           Populate the linked list with n random, but unique values. 
- *           Each value should be between 0 and 2^16 - 1.
- *           Then perform m random Member, Insert, and Delete operations on the link list.
- *           Let mMember, mInsert, and mDelete be the fractions of operations of each type.
- *           You may use any values within 0 and 2^16 – 1 while performing these three operations.
- *           However, to simplify the implementation, a new value inserted into the list cannot be a 
- *           value already in the list (it may be a value that was initially added to the list, but later removed).
- *
- * Compile:  gcc -g -Wall -o serial_linked_list serial_linked_list.c -pthread
- *           
- * Run:      ./serial_linked_list <n> <m> <mMember> <mInsert> <mDelete> 
- *           n is the number of initial unique values in the Link List.
- *           m is number of random Member, Insert, and Delete operations on the link list.
- *           mMember is the fractions of operations of Member operation.    
- *           mInsert is the fractions of operations of Insert operation.    
- *           mDelete is the fractions of operations of Delete operation.    
- *
- * Input:    none
- * 
- * Output:   This version prints the elapsed time required for
- *           single-threaded calculations.
- *
- * Notes:
- *          CS4532 Concurrent Programming 
- *          Take Home Lab 1 
- *          Due – Jun 28 before 11:55 PM  
- *
- * Date:    07/25/2014
- *
- * Author : Wijebandara WMNC
- *          100598M
- *          Computer Science and Engineering
- *
- * Email : chameerawijebandara@gmail.com 
- *              
- */       
- 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -162,7 +122,7 @@ void Get_args(int argc, char* argv[]) {
 }  
  
 void Usage(char* prog_name) {
-   fprintf(stderr, "usage: %s <N_hilos> <n> <m> <member> <insert> <delete>\n", prog_name);
+   fprintf(stderr, "usage: %s <n> <m> <member> <insert> <delete>\n", prog_name);
    fprintf(stderr,"n es la cantidad de elementos inicial en la lista.\n");
    fprintf(stderr,"m es la cantidad de operaciones a realizar entre los tres tipos.\n");
    fprintf(stderr,"member es la fraccion de operaciones member.\n");
